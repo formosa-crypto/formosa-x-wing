71092fde786e3997c02cc6906c93f12b8d2f7d7d
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-12-04 22:08:32 +0000

    Test x-wing using test vectors

--- code/crypto_kem/xwing/amd64/avx2/test/test_kem.c
+++ code/crypto_kem/xwing/amd64/avx2/test/test_kem.c
@@ -11,6 +11,8 @@ int main(void)
   unsigned char shk0[XWING_BYTES];
   unsigned char shk1[XWING_BYTES];
   int ss_ok = 0;
+  int pk_ok = 0;
+  int ct_ok = 0;
   
   unsigned char randomness0[MLKEM_SYMBYTES];
   unsigned char randomness1[2*MLKEM_SYMBYTES];
@@ -20,45 +22,40 @@ int main(void)
   fread(randomness1, 64, 1, urandom);
   fclose(urandom);
    
-  jade_kem_xwing_xwing_amd64_avx2_keypair_derand(pk0, sk0, randomness0);
+  jade_kem_xwing_xwing_amd64_avx2_keypair_derand(pk0, sk0, XWING_SEED_TEST_VECTOR);
     
-//  for(int i = 0; i < XWING_PUBLICKEYBYTES; i++) {
-//    if (pk0[i] != XWING_PUBLICKEY_TEST_VECTOR[i]) {
-//      printf("error crypto_kem_keypair pk: %d %d %d\n", i, pk0[i], XWING_PUBLICKEY_TEST_VECTOR[i]);    
-//      pk_ok = 1;
-//    }
-//  }
-// This will produce an error, even in formosa-mlkem
+  for(int i = 0; i < XWING_PUBLICKEYBYTES; i++) {
+    if (pk0[i] != XWING_PUBLICKEY_TEST_VECTOR[i]) {
+      printf("error crypto_kem_keypair pk: %d %d %d\n", i, pk0[i], XWING_PUBLICKEY_TEST_VECTOR[i]);    
+      pk_ok = 1;
+    }
+  }
 
-  // if(pk_ok == 0) printf("pk ok!\n\n");
-  // if(pk_ok == 1) printf("\npk BAD!\n\n");
-    
 
-  // if(sk_ok == 0) printf("sk ok!\n\n");
-  // if(sk_ok == 1) printf("\nsk BAD!\n\n");
-          
- 
-  jade_kem_xwing_xwing_amd64_avx2_enc_derand(ct0, shk0, pk0, randomness1);
-
-  //for(int i = 0; i < XWING_CIPHERTEXTBYTES; i++) {
-  //  if (ct0[i] != XWING_CIPHERTEXT_TEST_VECTOR[i]) {
-  //    printf("error crypto_kem_enc ct: %d %d %d\n", i, ct0[i], XWING_CIPHERTEXT_TEST_VECTOR[i]);
-  //    ct_ok = 1;
-  //  }
-  //}
-        
-  //if(ct_ok == 0) printf("ct ok!\n\n");
-  //if(ct_ok == 1) printf("\nct BAD!\n\n");
+ if(pk_ok == 0) printf("pk ok!\n\n");
+ if(pk_ok == 1) printf("\npk BAD!\n\n");
   
-  //for(int i = 0; i < XWING_BYTES; i++) {
-  //  if (shk0[i] != XWING_SHAREDSECRET_TEST_VECTOR[i]) {
-  //    printf("error crypto_kem_enc ss: %d %d %d\n", i, shk0[i], XWING_SHAREDSECRET_TEST_VECTOR[i]);    
-  //    ss_ok = 1;    
-  //  }
-  //}
+  jade_kem_xwing_xwing_amd64_avx2_enc_derand(ct0, shk0, XWING_PUBLICKEY_TEST_VECTOR, XWING_ESEED_TEST_VECTOR);
+
+  for(int i = 0; i < XWING_CIPHERTEXTBYTES; i++) {
+    if (ct0[i] != XWING_CIPHERTEXT_TEST_VECTOR[i]) {
+      printf("error crypto_kem_enc ct: %d %d %d\n", i, ct0[i], XWING_CIPHERTEXT_TEST_VECTOR[i]);
+      ct_ok = 1;
+    }
+  }
+        
+  if(ct_ok == 0) printf("ct ok!\n\n");
+  if(ct_ok == 1) printf("\nct BAD!\n\n");
+  
+  for(int i = 0; i < XWING_BYTES; i++) {
+    if (shk0[i] != XWING_SHAREDSECRET_TEST_VECTOR[i]) {
+      printf("error crypto_kem_enc ss: %d %d %d\n", i, shk0[i], XWING_SHAREDSECRET_TEST_VECTOR[i]);    
+      ss_ok = 1;    
+    }
+  }
       
-  //if(ss_ok == 0) printf("ss ok!\n\n");
-  //if(ss_ok == 1) printf("\nss BAD!\n\n");
+  if(ss_ok == 0) printf("ss (decaps) ok!\n\n");
+  if(ss_ok == 1) printf("\nss (decaps) BAD!\n\n");
 
 
   jade_kem_xwing_xwing_amd64_avx2_dec(shk1, ct0, sk0);
